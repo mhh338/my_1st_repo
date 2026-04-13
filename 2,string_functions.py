f4c1b29214bbdba23e30b6c056b9fb28369736f2
Muhammad Haashir <mh3162599@gmail.com>
2026-04-13 13:32:44 +0500

    Updating Project branch

--- 2,string_functions.py
+++
@@ -1,8 +0,0 @@
-a = "Hello World"
-print(a.upper())
-print(a.lower())
-print(a.find("Hello"))
-print(a.replace("World", "New World"))
-print(a.startswith("H"))
-print(a.endswith("ld"))
-print(a.count("l"))

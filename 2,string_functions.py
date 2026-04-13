0f7a7f0b04b4e037cd545650cf7292fc77dbe27f
Muhammad Haashir <mh3162599@gmail.com>
2026-04-13 10:45:35 +0500

    Adding more files to my repo

---
+++ 2,string_functions.py
@@ -0,0 +1,8 @@
+a = "Hello World"
+print(a.upper())
+print(a.lower())
+print(a.find("Hello"))
+print(a.replace("World", "New World"))
+print(a.startswith("H"))
+print(a.endswith("ld"))
+print(a.count("l"))

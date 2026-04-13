d51db7ebc5e129c574eeb35b2adb7472a07f1127
Muhammad Haashir <mh3162599@gmail.com>
2026-04-14 00:34:56 +0500

    Updating Project-Jarvis branch

--- Mega Project - JARVIS/main.py
+++ Mega Project - JARVIS/main.py
@@ -2,12 +2,14 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-recognizer = sr.Recognizer()
-engine = pyttsx3.init()
+# recognizer = sr.Recognizer()
 
-def speak(_text):
-    engine.say(_text)
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
     engine.runAndWait()
+
 
 def processCmd(_cmd):
     if "open google" in _cmd.lower():
@@ -17,24 +19,25 @@ def processCmd(_cmd):
 
 
 if __name__ == "__main__":
+    speak("Initializing...")
     while True:
-        speak("Initializing...")
         # Listening wake word 'Jarvis'
         # obtain audio from the microphone
         r = sr.Recognizer()
-        
-
         print("Recognizing...")
 
         # recognize speech using google
         try:
             with sr.Microphone() as source:
                 print("Listening!")
-                audio = r.listen(source, timeout=1.5, phrase_time_limit=1)
-            wakeWrd = r.recognize_google(audio)
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            wakeWrd = r.recognize_google(audio) 
             print(wakeWrd)
-            if(wakeWrd.lower() == "jarvis"):
-                speak("ya")
+            if wakeWrd.lower() == "jarvis":
+                # speak("ya")
+                print("ya bef")
+                speak("Yes sir, Jarvis is active.")
+                print("ya aft")
                 # Listening commandSphinx
                 with sr.Microphone() as source:
                     print("Jarvis Active...")
@@ -43,3 +46,4 @@ if __name__ == "__main__":
                     processCmd(command)      
         except Exception as e:
             print("Error; {0}".format(e))
+
